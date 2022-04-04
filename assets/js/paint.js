769ba2c882b5265fd1ded39f8577be08166d0dd4
Your Namegit config --global user.name Your <you@example.com>
2022-04-04 13:26:37 -0400

    Final Draft

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,71 +1,91 @@
 function configureListeners() {
-    let images = // select img elements  
+  let images = document.getElementsByTagName("img");
 
-
-     for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
-    } 
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseover", addOpacity);
+    images[i].addEventListener("mouseout", removeOpacity);
+  }
 }
 
 function addOpacity(event) {
-    // add appropriate CSS class
-    getProductInfo(event.target.id);     
+  if (!this.classList.contains("dim")) {
+    this.classList.add("dim");
+  }
+  getProductInfo(event.target.id);
 }
 
 function removeOpacity(event) {
-     //remove appropriate CSS class
+  if (this.classList.contains("dim")) {
+    this.classList.remove("dim");
+  }
 
-    let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
+  let element = document.getElementById("color-price");
+  element.textContent = "";
 
-    event.preventDefault();    
+  let color = document.getElementById("color-name");
+  color.textContent = "";
+
+  event.preventDefault();
 }
 
 function getProductInfo(paintColor) {
-    let price;
-    let colorName;  
-    
-    switch (paintColor) {
-        case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
-            break;           
-        case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
-            break;            
-        case 'pn3':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
-            break;   
-        case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
-            break;   
-        case 'pn7':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-        case 'pn8':
-            // set variables for price and color name and invoke a function to update the price   
-            break;   
-        case 'pn9':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-          default:              
-    }
+  let price;
+  let colorName;
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = // select element with corresponding id
-        // display price
-        
-        let color = // select element with corresponding id
-        //display color name
-    }
-    
+  switch (paintColor) {
+    case "pn1":
+      price = "$19.99";
+      colorName = "Lime Green";
+      updatePrice(colorName, price);
+      break;
+    case "pn2":
+      price = "$12.99";
+      colorName = "Medium Brown";
+      updatePrice(colorName, price);
+      break;
+    case "pn3":
+      price = "$11.99";
+      colorName = "Royal Blue";
+      updatePrice(colorName, price);
+      break;
+    case "pn4":
+      price = "$14.99";
+      colorName = "Bright Red";
+      updatePrice(colorName, price);
+      break;
+    case "pn5":
+      price = "$9.99";
+      colorName = "Solid White";
+      updatePrice(colorName, price);
+      break;
+    case "pn6":
+      price = "$15.99";
+      colorName = "Solid Black";
+      updatePrice(colorName, price);
+      break;
+    case "pn7":
+      price = "$8.99";
+      colorName = "Medium Blue";
+      updatePrice(colorName, price);
+      break;
+    case "pn8":
+      price = "$16.99";
+      colorName = "Light Purple";
+      updatePrice(colorName, price);
+      break;
+    case "pn9":
+      price = "$17.99";
+      colorName = "Bright Yellow";
+      updatePrice(colorName, price);
+      break;
+    default:
+  }
+
+  function updatePrice(colorName, price) {
+    let colorPrice = document.getElementById("color-price");
+    colorPrice.textContent = price;
+
+    let color = document.getElementById("color-name");
+    color.textContent = colorName;
+  }
 }
